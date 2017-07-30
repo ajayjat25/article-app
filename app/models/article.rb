4ba554fdf0e7585664c687fecb16def2692b5b53
shashi-we <shashi.we@gmail.com>
2017-07-30 15:46:45 +0530

    working on article app

---
+++ app/models/article.rb
@@ -0,0 +1,7 @@
+class Article < ApplicationRecord
+  validates :title, :description, presence:true
+  validates :title,length:{minimum: 5}
+  extend FriendlyId
+  friendly_id :title, use: [:slugged, :finders]
+  belongs_to :user
+end
